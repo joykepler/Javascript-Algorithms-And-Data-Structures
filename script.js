c68ee919d6cb8022874ed2977d481c07028957a7
joykepler <joyjoykepler@gmail.com>
2024-01-03 20:43:04 -0300

    function

--- script.js
+++ script.js
@@ -23,3 +23,7 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function goStore() {
+  
+}
