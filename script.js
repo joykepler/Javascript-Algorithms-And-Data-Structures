3796b997c3e788ad91bcf9ef5bbf0aceb8190cd6
joykepler <joyjoykepler@gmail.com>
2024-01-03 20:37:11 -0300

    comment

--- script.js
+++ script.js
@@ -17,3 +17,5 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+//initialize buttons
