73aaa17aa35d35a74811ac81d01802ad8b0deab5
joykepler <joyjoykepler@gmail.com>
2024-01-04 20:50:28 -0300

    gostore

--- script.js
+++ script.js
@@ -24,8 +24,10 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-  function goStore() {
+function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 }
 
   function goCave() {
