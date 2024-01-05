8c53c08e1aa03b78ffbf4a1690ac5e4229c1bb86
joykepler <joyjoykepler@gmail.com>
2024-01-04 21:29:04 -0300

    name const

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-const locations = [{}];
+
+const locations = [
+  {
+    name: "town square"
+  }
+];
 
 // initialize buttons
 button1.onclick = goStore;
