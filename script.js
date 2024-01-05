2eed07e8d8a779ea5cb55cbcb4f3f41efb5c3faf
joykepler <joyjoykepler@gmail.com>
2024-01-04 21:40:31 -0300

    loactions

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [
   {
     name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"]
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon]
   }
 ];
 
