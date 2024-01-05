38e7f135e542400d8e18ff2f545084ea682e7598
joykepler <joyjoykepler@gmail.com>
2024-01-04 21:02:01 -0300

    functions

--- script.js
+++ script.js
@@ -42,3 +42,15 @@ function goStore() {
   function fightDragon() {
     console.log("Fighting dragon.")
   } 
+
+  function buyHealth() {
+
+  }
+  
+  function buyWeapon() {
+    
+  }
+  
+  function goTown() {
+    
+  }
