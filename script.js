08a64e1a6825fc6ec6cf54be00ae0629e9921941
joykepler <joyjoykepler@gmail.com>
2024-01-04 21:04:14 -0300

    gotown

--- script.js
+++ script.js
@@ -24,6 +24,17 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
+
+
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
@@ -31,26 +42,21 @@ function goStore() {
   button1.onclick = buyHealth;
   button2.onclick = buyWeapon;
   button3.onclick = goTown;
-
   text.innerText = "You enter the store.";
 }
 
-  function goCave() {
-    console.log("Going to cave.");
-  }
+function goCave() {
+  console.log("Going to cave.");
+}
 
-  function fightDragon() {
-    console.log("Fighting dragon.")
-  } 
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
 
-  function buyHealth() {
+function buyHealth() {
 
-  }
-  
-  function buyWeapon() {
-    
-  }
-  
-  function goTown() {
-    
-  }
+}
+
+function buyWeapon() {
+
+}
